f3f473ca5b1ecfd6caa72f6e7314bbdf0928f444
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2019-02-25 21:15:06 -0600

    Use onClickListeners to open views

--- app/src/main/java/com/alaythiaproductions/musicapp/MainActivity.java
+++ app/src/main/java/com/alaythiaproductions/musicapp/MainActivity.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,25 +14,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void openKnucklePuck(View view) {
-        Intent intent = new Intent(MainActivity.this, KnucklePuck.class);
-        startActivity(intent);
-    }
 
-    public void openDanceGavinDance(View view){
-        Intent intent = new Intent(MainActivity.this, DanceGavinDance.class);
-        startActivity(intent);
-    }
+        TextView knucklePuckTextView = (TextView)findViewById(R.id.knuckle_puck);
+        TextView danceGavinDanceTextView = (TextView)findViewById(R.id.dance_gavin_dance);
+        TextView emarosaTextView = (TextView)findViewById(R.id.emarosa);
+        TextView foreverCameCallingTextView = (TextView)findViewById(R.id.forever_came_calling);
 
-    public void openForeverCameCalling(View view){
-        Intent intent = new Intent(MainActivity.this, ForeverCameCalling.class);
-        startActivity(intent);
-    }
+        knucklePuckTextView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, KnucklePuck.class);
+                startActivity(intent);
+            }
+        });
 
-    public void openEmarosa(View view){
-        Intent intent = new Intent(MainActivity.this, Emarosa.class);
-        startActivity(intent);
+        danceGavinDanceTextView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, DanceGavinDance.class);
+                startActivity(intent);
+            }
+        });
+
+        emarosaTextView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Emarosa.class);
+                startActivity(intent);
+            }
+        });
+
+        foreverCameCallingTextView.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ForeverCameCalling.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
