788537c4b6c9afd1a9c7aafa836e0b27b78b2a47
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2019-02-25 21:03:38 -0600

    Add onClick to Open Views

--- app/src/main/java/com/alaythiaproductions/musicapp/MainActivity.java
+++ app/src/main/java/com/alaythiaproductions/musicapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.alaythiaproductions.musicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openKnucklePuck(View view) {
+        Intent intent = new Intent(MainActivity.this, KnucklePuck.class);
+        startActivity(intent);
+    }
+
+    public void openDanceGavinDance(View view){
+        Intent intent = new Intent(MainActivity.this, DanceGavinDance.class);
+        startActivity(intent);
+    }
+
+    public void openForeverCameCalling(View view){
+        Intent intent = new Intent(MainActivity.this, ForeverCameCalling.class);
+        startActivity(intent);
+    }
+
+    public void openEmarosa(View view){
+        Intent intent = new Intent(MainActivity.this, Emarosa.class);
+        startActivity(intent);
     }
 }
