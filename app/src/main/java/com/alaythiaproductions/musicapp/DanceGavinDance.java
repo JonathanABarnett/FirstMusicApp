26de181b3e8d4bd42eb0713de39f48be8811bc56
Jonathan Barnett <jonathan.a.barnett@gmail.com>
2019-02-25 20:51:56 -0600

    Basic Layout

---
+++ app/src/main/java/com/alaythiaproductions/musicapp/DanceGavinDance.java
@@ -0,0 +1,13 @@
+package com.alaythiaproductions.musicapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DanceGavinDance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dance_gavin_dance);
+    }
+}
---
+++ app/src/main/java/com/alaythiaproductions/musicapp/KnucklePuck.java
@@ -0,0 +1,13 @@
+package com.alaythiaproductions.musicapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KnucklePuck extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_knuckle_puck);
+    }
+}
